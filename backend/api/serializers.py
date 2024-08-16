76bb2c62472dcf119facdeba43f1ce60b77998b4
VibavariG <vibavari2301@gmail.com>
2024-08-16 06:50:22 -0400

    create user view, urls for token and refreshtoken

---
+++ backend/api/serializers.py
@@ -0,0 +1,18 @@
+# something that takes in a python obj and convert into json 
+# data that can be used in communication with other applications
+# json <-> python
+#user model is built into django and represents a user model
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}   #we only want to accept password when we create a new user, not return it when we return a user
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data) #** to split up the key:values in dict
+        return user
+    
