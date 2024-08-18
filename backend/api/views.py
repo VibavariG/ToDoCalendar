c4792e297e5f28fe5d4bcbc855def01e57f7343b
VibavariG <vibavari2301@gmail.com>
2024-08-18 18:21:04 -0400

    add functionality to create notes

--- backend/api/views.py
+++ backend/api/views.py
@@ -17,3 +17,15 @@ class NoteListCreate(generics.ListCreateAPIView):
     def get_queryset(self):
         user = self.request.user
         return Note.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    
