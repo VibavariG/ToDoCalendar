3f779fc232c6add198b67b8286b6e8c731f83962
VibavariG <vibavari2301@gmail.com>
2024-08-23 18:35:03 -0400

    customize not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 function NotFound() {
     return <div>
         <h1>404 Not Found</h1>
-        <p>The oage you're looking for does not exist!</p>
+        <p>The page you're looking for does not exist!</p>
     </div>
 }
 
