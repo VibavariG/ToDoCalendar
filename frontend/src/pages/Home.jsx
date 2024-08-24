90aae357f027dee429b3e25cc05273dd3ca74aaa
VibavariG <vibavari2301@gmail.com>
2024-08-24 09:42:06 -0400

    BFE-39: Add html form to create note

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -27,9 +27,9 @@ function Home() {
       .then((res) => {
         if (res.status === 204) alert("Note deleted!");
         else alert("Failed to delete note.");
+        getNotes();
       })
       .catch((error) => alert(error));
-    getNotes();
   };
 
   const createNote = (e) => {
@@ -39,12 +39,42 @@ function Home() {
       .then((res) => {
         if (res.status === 201) alert("Note created!");
         else alert("Failed to create note.");
+        getNotes();
       })
       .catch((err) => alert(err));
-    getNotes();
   };
 
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>
+        <h2>Notes</h2>
+      </div>
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="content">Content:</label>
+        <br />
+        <textarea
+          id="content"
+          name="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Home;
